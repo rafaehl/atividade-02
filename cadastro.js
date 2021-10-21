c3b461a47105a4fc95349589fc88d85ac7fa8df0
rafahel <el_fael@hotmail.com>
2021-10-20 22:42:28 -0300

    cadastro de participante - atividade 02

---
+++ cadastro.js
@@ -0,0 +1,43 @@
+let nomeParticipante = "Ricardo";
+if (nomeParticipante.length>= 3) {
+    console.log ("Campo nome válido")
+    
+} else {
+    console.log("Campo nome inválido");
+    
+}
+
+let emailParticipante = "ricardo@hotmail.com";
+if (emailParticipante.includes ("@")) {
+    console.log ("Campo email válido")
+    
+} else {
+    console.log("Campo email inválido");
+    
+}
+let idade = (19);
+if (idade >= 18) {
+    console.log ("Idade compativel com a faixa etária requerida, pode prosseguir")
+    }else {
+     console.log ("Idade imcopativel. Evento apenas para maiores de 18 anos");
+    }
+
+let dataEvento = (20/11/2021);
+if (dataEvento < hoje) {
+    console.log("Data invalida")
+    
+} else { 
+    console.log ("Data válida");
+
+let listaPalestrante = ["palestrante1, palestrante2, palestrante3, palestrante4, palestrante5"];
+let quantidadePalestrante = listaPalestrante.length;
+}
+let listaParticipantes = ["participante1, participante2, participante3, participante4, participante5"];
+if (listaParticipante < 100) {
+    console.log ("Há vagas disponíveis")
+    
+} else {
+    console.log ("Não há mais vagas disponíveis")
+}
+    
+}
